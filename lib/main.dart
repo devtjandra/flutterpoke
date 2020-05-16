1930b3c8205821ffb679032cced4a7f29652f1a0
Devina <devtjandra@gmail.com>
2020-05-16 12:29:10 +0800

    Some final things

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Poke',
         home: HomePage(),
         theme: ThemeData(
             accentColor: AppColors.primary, primaryColor: AppColors.primary));
